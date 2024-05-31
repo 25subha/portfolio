cbbfa692dcdbceaca553205ecb659a6f775b4fa9
subhankar <mrsubhankar25@gmail.com>
2024-05-31 22:11:53 +0530

    add flex basis

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,16 +11,16 @@ const About = () => {
   }
   return (
     <div  name='About' className=' max-w-screen-2xl container mx-auto px-4 md:px-20  ' >
-      <div  name='Skills' className='flex flex-col items-center'>
+      <div   className='flex flex-col items-center '>
       <h1 className='font-[500] text-[30px] text-center mt-12 mb-2'>About Me</h1>
       <div className='h-1 w-24 bg-green-500'></div>
     </div>
-      <div className=' flex mt-12 justify-between items-center flex-wrap '>
+      <div className=' flex mt-12 justify-between md:items-start  items-center flex-wrap '>
         
-      <div className='md:basis-[35%] basis-full p-4'>
-        <img src={subhankar} className='  h-[290px] w-[260px] md:h-[250px] md:w-[210px]   m-auto rounded-md block  transition-transform duration-[0.5s] hover:transform hover:scale-[1.05] ' alt="" />
+      <div className='md:basis-[20%] basis-full p-4 '>
+        <img src={subhankar} className='h-[290px] w-[260px] md:h-[250px] md:w-[210px]   m-auto rounded-md block  transition-transform duration-[0.5s] hover:transform hover:scale-[1.05] ' alt="" />
       </div>
-      <div className=' md:basis-[60%] basis-full'>
+      <div className=' md:basis-[70%] basis-full'>
         <p className='text-gray-300 p-4  '>
         Hello there! I'm subhankar maji, and i'm a full-stack web devoloper. I'm very passionate and dedicated to my work. I have acquired the skills and knowledge necessary to make your project a success. I enjoyed every step of the design process, form discusssion and collaboration. 
         </p>
